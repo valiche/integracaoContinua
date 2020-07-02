10986306469203a358931e098e470654556b83a3
Danilo <didox_59@yahoo.com.br>
2020-07-02 14:06:04 -0300

    correcao cypress bdd

--- cypress/integration/cliente.spec.js
+++ cypress/integration/cliente.spec.js
@@ -2,14 +2,10 @@
  
 context('testar Home Page', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3000')
+    cy.visit('/')
   })
 
-  // https://on.cypress.io/interacting-with-elements
-
   it('deveria mostrar a lista de clientes', () => {
-    // https://on.cypress.io/type
     cy.get('li').should("to.have.length", 3)
-      
   })
 })
